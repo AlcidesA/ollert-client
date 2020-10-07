00b3bbfc7353710e7b99a4a8bb7d8e197c0d73ec
Alcides A <aaugusto.kd@gmail.com>
2020-10-06 22:45:26 -0300

    add request interceptor to add user token

--- src/providers/Api.ts
+++ src/providers/Api.ts
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import User from './User'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
+})
+
+api.interceptors.request.use((config) => {
+  const token = User.get().token
+
+  config.headers.Authorization = 'Bearer ' + token
+
+  return config
 })
 
 export default api
