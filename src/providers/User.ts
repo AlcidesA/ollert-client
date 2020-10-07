b21706870484a063de36f81a3c6903419f294aca
Alcides A <aaugusto.kd@gmail.com>
2020-10-06 22:42:54 -0300

    add get stored user

--- src/providers/User.ts
+++ src/providers/User.ts
@@ -1,7 +1,22 @@
+interface AuthUser {
+  token: string;
+  user: {
+    id: number;
+    email: string;
+    name: string
+  }
+}
+
 export default class User {
   static isAuthenticated (): boolean {
     const user = localStorage.getItem('user')
 
     return !!user
+  }
+
+  static get(): AuthUser {
+    const user = localStorage.getItem('user')
+
+    return JSON.parse(user ?? "{}")
   }
 }
